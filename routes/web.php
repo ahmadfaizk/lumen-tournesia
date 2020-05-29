bd6f07da2b807cef0c3945a4a74730d501976bea
Ahmad Faiz Kamaludin <ach.faiz.k@gmail.com>
2020-05-29 21:06:34 +0700

    updates namespases in routes

--- routes/web.php
+++ routes/web.php
@@ -15,7 +15,7 @@ $router->get('/', function () use ($router) {
     return $router->app->version();
 });
 
-$router->group(['prefix' => 'api'], function() use ($router) {
+$router->group(['prefix' => 'api/v1/'], function() use ($router) {
 
     $router->post('register', 'AuthController@register');
     $router->post('login', 'AuthController@login');
